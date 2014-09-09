5b451923e425d48a6f5775a3ff413d303ed89a7a
Liang Ding <DL88250@gmail.com>
2014-09-09 11:23:49 +0800

    测试项目

---
+++ data/workspace/src/hello/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"go/ast"
+	"go/parser"
+	"go/token"
+)
+
+func main() {
+	// src is the input for which we want to print the AST.
+	src := `
+package main
+func main() {f
+}
+`
+
+	// Create the AST by parsing src.
+	fset := token.NewFileSet() // positions are relative to fset
+	f, err := parser.ParseFile(fset, "", src, 0)
+	if err != nil {
+		panic(err)
+	}
+
+	// Print the AST.
+	ast.Print(fset, f)
+
+}
