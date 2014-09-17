7abb8b978c4e7e72169af4182cbe159cedcde3ec
Liang Ding <DL88250@gmail.com>
2014-09-17 16:27:45 +0800

    默认的测试项目

---
+++ data/user_workspaces/admin/src/mytest/time/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"mytest/time/pkg"
+	"time"
+)
+
+func main() {
+	for i := 0; i < 5; i++ {
+		fmt.Println("Hello, 世界", pkg.Now())
+
+		time.Sleep(time.Second)       
+        time.
+	}
+
+}
---
+++ data/user_workspaces/admin/src/mytest/time/pkg/time.go
@@ -0,0 +1,17 @@
+package pkg
+
+import (
+	"time"
+)
+
+func Now() time.Time {
+	return time.Now()
+}
+
+func Now1() time.Time {
+	return time.Now()
+}
+
+func Now2() {
+    
+}
