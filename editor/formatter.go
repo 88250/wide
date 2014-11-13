93f6958b9c669d936b4dcc6a8a83419d9bc4f4ec
Liang Ding <dl88250@gmail.com>
2014-11-13 22:28:28 +0800

    go formmat

--- editor/formatter.go
+++ editor/formatter.go
@@ -1,11 +1,11 @@
 // Copyright (c) 2014, B3log
-//  
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//  
+//
 //     http://www.apache.org/licenses/LICENSE-2.0
-//  
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -84,7 +84,9 @@ func GoFmtHandler(w http.ResponseWriter, r *http.Request) {
 	bytes, _ := cmd.Output()
 	output := string(bytes)
 	if "" == output {
-		data["succ"] = false
+		// format error, returns the original content
+		data["succ"] = true
+		data["code"] = code
 
 		return
 	}
