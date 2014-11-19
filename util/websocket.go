eb8e5133ab449755d8205ee42884225eadc71167
Liang Ding <dl88250@gmail.com>
2014-11-20 00:17:59 +0800

    fix npd

--- util/websocket.go
+++ util/websocket.go
@@ -1,11 +1,11 @@
 // Copyright (c) 2014, B3log
-//  
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//  
+//
 //     http://www.apache.org/licenses/LICENSE-2.0
-//  
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -31,7 +31,9 @@ type WSChannel struct {
 
 // Close closed the channel.
 func (c *WSChannel) Close() {
-	c.Conn.Close()
+	if nil != c.Conn {
+		c.Conn.Close()
+	}
 }
 
 // Refresh refreshes the channel by updating its use time.
