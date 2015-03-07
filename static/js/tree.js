fcb529cfe3fbbd4409afd668afab8c8dad904b20
Van <lly219@gmail.com>
2015-03-07 17:51:28 +0800

    #191
    add context menu on directory

--- static/js/tree.js
+++ static/js/tree.js
@@ -179,6 +179,15 @@ var tree = {
 
         tree.fileTree.reAsyncChildNodes(wide.curNode, "refresh", true);
     },
+    gitClone: function (it) {
+        if (it) {
+            if ($(it).hasClass("disabled")) {
+                return false;
+            }
+        }
+
+        alert("dev");
+    },
     import: function () {
         var request = newWideRequest();
         request.path = wide.curNode.path;
