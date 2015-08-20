eaa93281d1e20cf1c88bc4a8ca7166585b423cdf
Liang Ding <DL88250@gmail.com>
2015-08-20 21:47:00 +0800

    .

--- pkg.sh
+++ pkg.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Wide package tool.
-# 
+#
 # Command: 
 #  ./pkg.sh ${version} ${target}
 # Example:
@@ -19,69 +19,63 @@ echo target=${target}
 ## darwin
 os=darwin
 
+echo wide-${ver}-${GOOS}-${GOARCH}.tar.gz
 export GOOS=${os}
 export GOARCH=386
 go build
 go build github.com/visualfc/gotools
 go build github.com/nsf/gocode
-cp ${GOPATH}/bin/${GOOS}_${GOARCH}/gotools .
-cp ${GOPATH}/bin/${GOOS}_${GOARCH}/gocode .
 tar zcvf ${target}/wide-${ver}-${GOOS}-${GOARCH}.tar.gz ${list} gotools gocode wide --exclude-vcs --exclude='conf/*.go' --exclude='i18n/*.go'
 rm -f wide gotools gocode
 
+echo wide-${ver}-${GOOS}-${GOARCH}.tar.gz
 export GOOS=${os}
 export GOARCH=amd64
 go build
 go build github.com/visualfc/gotools
 go build github.com/nsf/gocode
-cp ${GOPATH}/bin/${GOOS}_${GOARCH}/gotools .
-cp ${GOPATH}/bin/${GOOS}_${GOARCH}/gocode .
-tar zcvf ${target}/wide-${ver}-${GOOS}-${GOARCH}.tar.gz ${list} gotools gocode wide --exclude-vcs --exclude='conf/*.go' --exclude='i18n/*.go'
+tar zvf ${target}/wide-${ver}-${GOOS}-${GOARCH}.tar.gz ${list} gotools gocode wide --exclude-vcs --exclude='conf/*.go' --exclude='i18n/*.go'
 rm -f wide gotools gocode
 
 ## linux
 os=linux
 
+echo wide-${ver}-${GOOS}-${GOARCH}.tar.gz
 export GOOS=${os}
 export GOARCH=386
 go build
 go build github.com/visualfc/gotools
 go build github.com/nsf/gocode
-cp ${GOPATH}/bin/${GOOS}_${GOARCH}/gotools .
-cp ${GOPATH}/bin/${GOOS}_${GOARCH}/gocode .
-tar zcvf ${target}/wide-${ver}-${GOOS}-${GOARCH}.tar.gz ${list} gotools gocode wide --exclude-vcs --exclude='conf/*.go' --exclude='i18n/*.go'
+tar zvf ${target}/wide-${ver}-${GOOS}-${GOARCH}.tar.gz ${list} gotools gocode wide --exclude-vcs --exclude='conf/*.go' --exclude='i18n/*.go'
 rm -f wide gotools gocode
 
+echo wide-${ver}-${GOOS}-${GOARCH}.tar.gz
 export GOOS=${os}
 export GOARCH=amd64
 go build
 go build github.com/visualfc/gotools
 go build github.com/nsf/gocode
-cp ${GOPATH}/bin/${GOOS}_${GOARCH}/gotools .
-cp ${GOPATH}/bin/${GOOS}_${GOARCH}/gocode .
-tar zcvf ${target}/wide-${ver}-${GOOS}-${GOARCH}.tar.gz ${list} gotools gocode wide --exclude-vcs --exclude='conf/*.go' --exclude='i18n/*.go'
+tar zvf ${target}/wide-${ver}-${GOOS}-${GOARCH}.tar.gz ${list} gotools gocode wide --exclude-vcs --exclude='conf/*.go' --exclude='i18n/*.go'
 rm -f wide gotools gocode
 
 ## windows
 os=windows
 
+echo wide-${ver}-${GOOS}-${GOARCH}.zip
 export GOOS=${os}
 export GOARCH=386
 go build
 go build github.com/visualfc/gotools
 go build github.com/nsf/gocode
-cp ${GOPATH}/bin/${GOOS}_${GOARCH}/gotools.exe .
-cp ${GOPATH}/bin/${GOOS}_${GOARCH}/gocode.exe .
-zip -r ${target}/wide-${ver}-${GOOS}-${GOARCH}.zip ${list} gotools.exe gocode.exe wide.exe --exclude=conf/*.go --exclude=i18n/*.go
+zip -rq ${target}/wide-${ver}-${GOOS}-${GOARCH}.zip ${list} gotools.exe gocode.exe wide.exe --exclude=conf/*.go --exclude=i18n/*.go
 rm -f wide.exe gotools.exe gocode.exe
 
+echo wide-${ver}-${GOOS}-${GOARCH}.zip
 export GOOS=${os}
 export GOARCH=amd64
 go build
 go build github.com/visualfc/gotools
 go build github.com/nsf/gocode
-cp ${GOPATH}/bin/${GOOS}_${GOARCH}/gotools.exe .
-cp ${GOPATH}/bin/${GOOS}_${GOARCH}/gocode.exe .
-zip -r ${target}/wide-${ver}-${GOOS}-${GOARCH}.zip ${list} gotools.exe gocode.exe wide.exe --exclude=conf/*.go --exclude=i18n/*.go
+zip -rq ${target}/wide-${ver}-${GOOS}-${GOARCH}.zip ${list} gotools.exe gocode.exe wide.exe --exclude=conf/*.go --exclude=i18n/*.go
 rm -f wide.exe gotools.exe gocode.exe
 
