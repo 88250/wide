3a3ffb1bc4396f9b15866df08de350d349e85ab1
Liang Ding <dl88250@gmail.com>
2016-02-04 15:16:05 +0800

    Release Wide 1.5.0

--- conf/wide.go
+++ conf/wide.go
@@ -39,7 +39,7 @@ const (
 	PathListSeparator = string(os.PathListSeparator)
 
 	// WideVersion holds the current Wide's version.
-	WideVersion = "1.4.0"
+	WideVersion = "1.5.0"
 	// CodeMirrorVer holds the current editor version.
 	CodeMirrorVer = "5.1"
 
