3666c12738469c245d3a3208d6cec3ef3747e69b
Van <lly219@gmail.com>
2018-10-05 23:11:52 +0800

    :arrow_up: upgrade gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2014-2015, b3log.org
+ * Copyright (c) 2014-2018, b3log.org
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,120 +16,135 @@
 
 /**
  * @file frontend tool.
- * 
+ *
  * @author <a href="mailto:liliyuan@fangstar.net">Liyuan Li</a>
- * @version 0.1.0.0, Dec 15, 2015 
+ * @version 0.2.0.0, Oct 5, 2018
  */
-var gulp = require("gulp");
-var concat = require('gulp-concat');
-var minifyCSS = require('gulp-minify-css');
-var uglify = require('gulp-uglify');
-var sourcemaps = require("gulp-sourcemaps");
+var gulp = require('gulp')
+var concat = require('gulp-concat')
+var minifyCSS = require('gulp-minify-css')
+var uglify = require('gulp-uglify')
+var sourcemaps = require('gulp-sourcemaps')
 
-gulp.task('cc', function () {
-    // css
-    var cssLibs = ['./static/js/lib/jquery-layout/layout-default-latest.css',
-        './static/js/lib/codemirror-5.1/codemirror.css',
-        './static/js/lib/codemirror-5.1/addon/hint/show-hint.css',
-        './static/js/lib/codemirror-5.1/addon/lint/lint.css',
-        './static/js/lib/codemirror-5.1/addon/fold/foldgutter.css',
-        './static/js/lib/codemirror-5.1/addon/dialog/dialog.css',
-        './static/js/overwrite/codemirror/theme/*.css'];
-    gulp.src(cssLibs)
-            .pipe(minifyCSS())
-            .pipe(concat('lib.min.css'))
-            .pipe(gulp.dest('./static/css/'));
+function minLibCSS () {
+  // css
+  var cssLibs = [
+    './static/js/lib/jquery-layout/layout-default-latest.css',
+    './static/js/lib/codemirror-5.1/codemirror.css',
+    './static/js/lib/codemirror-5.1/addon/hint/show-hint.css',
+    './static/js/lib/codemirror-5.1/addon/lint/lint.css',
+    './static/js/lib/codemirror-5.1/addon/fold/foldgutter.css',
+    './static/js/lib/codemirror-5.1/addon/dialog/dialog.css',
+    './static/js/overwrite/codemirror/theme/*.css']
+  return gulp.src(cssLibs).
+    pipe(minifyCSS()).
+    pipe(concat('lib.min.css')).
+    pipe(gulp.dest('./static/css/'))
+}
 
-    gulp.src('./static/js/lib/ztree/zTreeStyle.css')
-            .pipe(minifyCSS())
-            .pipe(concat('zTreeStyle.min.css'))
-            .pipe(gulp.dest('./static/js/lib/ztree/'));
+function minZTreeStyleCSS () {
+  return gulp.src('./static/js/lib/ztree/zTreeStyle.css').
+    pipe(minifyCSS()).
+    pipe(concat('zTreeStyle.min.css')).
+    pipe(gulp.dest('./static/js/lib/ztree/'))
+}
 
-    var cssWide = ['./static/css/dialog.css',
-        './static/css/base.css',
-        './static/css/wide.css',
-        './static/css/side.css',
-        './static/css/start.css',
-        './static/css/about.css'
-    ];
+function minWideCSS () {
+  var cssWide = [
+    './static/css/dialog.css',
+    './static/css/base.css',
+    './static/css/wide.css',
+    './static/css/side.css',
+    './static/css/start.css',
+    './static/css/about.css',
+  ]
 
-    gulp.src(cssWide)
-            .pipe(minifyCSS())
-            .pipe(concat('wide.min.css'))
-            .pipe(gulp.dest('./static/css/'));
+  return gulp.src(cssWide).
+    pipe(minifyCSS()).
+    pipe(concat('wide.min.css')).
+    pipe(gulp.dest('./static/css/'))
+}
 
+function minLibJS () {
+  // js
+  var jsLibs = [
+    './static/js/lib/jquery-2.1.1.min.js',
+    './static/js/lib/jquery-ui.min.js',
+    './static/js/lib/jquery-layout/jquery.layout-latest.js',
+    './static/js/lib/reconnecting-websocket.js',
+    './static/js/lib/Autolinker.min.js',
+    './static/js/lib/emmet.js',
+    './static/js/lib/js-beautify-1.5.4/beautify.js',
+    './static/js/lib/js-beautify-1.5.4/beautify-html.js',
+    './static/js/lib/js-beautify-1.5.4/beautify-css.js',
+    './static/js/lib/jquery-file-upload-9.8.0/vendor/jquery.ui.widget.js',
+    './static/js/lib/jquery-file-upload-9.8.0/jquery.iframe-transport.js',
+    './static/js/lib/jquery-file-upload-9.8.0/jquery.fileupload.js',
+    './static/js/lib/codemirror-5.1/codemirror.min.js',
+    './static/js/lib/codemirror-5.1/addon/lint/lint.js',
+    './static/js/lib/codemirror-5.1/addon/lint/json-lint.js',
+    './static/js/lib/codemirror-5.1/addon/selection/active-line.js',
+    './static/js/lib/codemirror-5.1/addon/selection/active-line.js',
+    './static/js/overwrite/codemirror/addon/hint/show-hint.js',
+    './static/js/lib/codemirror-5.1/addon/hint/anyword-hint.js',
+    './static/js/lib/codemirror-5.1/addon/display/rulers.js',
+    './static/js/lib/codemirror-5.1/addon/edit/closebrackets.js',
+    './static/js/lib/codemirror-5.1/addon/edit/matchbrackets.js',
+    './static/js/lib/codemirror-5.1/addon/edit/closetag.js',
+    './static/js/lib/codemirror-5.1/addon/search/searchcursor.js',
+    './static/js/lib/codemirror-5.1/addon/search/search.js',
+    './static/js/lib/codemirror-5.1/addon/dialog/dialog.js',
+    './static/js/lib/codemirror-5.1/addon/search/match-highlighter.js',
+    './static/js/lib/codemirror-5.1/addon/fold/foldcode.js',
+    './static/js/lib/codemirror-5.1/addon/fold/foldgutter.js',
+    './static/js/lib/codemirror-5.1/addon/fold/brace-fold.js',
+    './static/js/lib/codemirror-5.1/addon/fold/xml-fold.js',
+    './static/js/lib/codemirror-5.1/addon/fold/markdown-fold.js',
+    './static/js/lib/codemirror-5.1/addon/fold/comment-fold.js',
+    './static/js/lib/codemirror-5.1/addon/mode/loadmode.js',
+    './static/js/lib/codemirror-5.1/addon/comment/comment.js',
+    './static/js/lib/codemirror-5.1/mode/meta.js',
+    './static/js/lib/codemirror-5.1/mode/go/go.js',
+    './static/js/lib/codemirror-5.1/mode/clike/clike.js',
+    './static/js/lib/codemirror-5.1/mode/xml/xml.js',
+    './static/js/lib/codemirror-5.1/mode/htmlmixed/htmlmixed.js',
+    './static/js/lib/codemirror-5.1/mode/javascript/javascript.js',
+    './static/js/lib/codemirror-5.1/mode/markdown/markdown.js',
+    './static/js/lib/codemirror-5.1/mode/css/css.js',
+    './static/js/lib/codemirror-5.1/mode/shell/shell.js',
+    './static/js/lib/codemirror-5.1/mode/sql/sql.js',
+    './static/js/lib/codemirror-5.1/keymap/vim.js',
+    './static/js/lib/lint/json-lint.js',
+    './static/js/lib/lint/go-lint.js']
+  return gulp.src(jsLibs).
+    pipe(uglify()).
+    pipe(concat('lib.min.js')).
+    pipe(gulp.dest('./static/js/'))
+}
 
-    // js
-    var jsLibs = ['./static/js/lib/jquery-2.1.1.min.js',
-        './static/js/lib/jquery-ui.min.js',
-        './static/js/lib/jquery-layout/jquery.layout-latest.js',
-        './static/js/lib/reconnecting-websocket.js',
-        './static/js/lib/Autolinker.min.js',
-        './static/js/lib/emmet.js',
-        './static/js/lib/js-beautify-1.5.4/beautify.js',
-        './static/js/lib/js-beautify-1.5.4/beautify-html.js',
-        './static/js/lib/js-beautify-1.5.4/beautify-css.js',
-        './static/js/lib/jquery-file-upload-9.8.0/vendor/jquery.ui.widget.js',
-        './static/js/lib/jquery-file-upload-9.8.0/jquery.iframe-transport.js',
-        './static/js/lib/jquery-file-upload-9.8.0/jquery.fileupload.js',
-        './static/js/lib/codemirror-5.1/codemirror.min.js',
-        './static/js/lib/codemirror-5.1/addon/lint/lint.js',
-        './static/js/lib/codemirror-5.1/addon/lint/json-lint.js',
-        './static/js/lib/codemirror-5.1/addon/selection/active-line.js',
-        './static/js/lib/codemirror-5.1/addon/selection/active-line.js',
-        './static/js/overwrite/codemirror/addon/hint/show-hint.js',
-        './static/js/lib/codemirror-5.1/addon/hint/anyword-hint.js',
-        './static/js/lib/codemirror-5.1/addon/display/rulers.js',
-        './static/js/lib/codemirror-5.1/addon/edit/closebrackets.js',
-        './static/js/lib/codemirror-5.1/addon/edit/matchbrackets.js',
-        './static/js/lib/codemirror-5.1/addon/edit/closetag.js',
-        './static/js/lib/codemirror-5.1/addon/search/searchcursor.js',
-        './static/js/lib/codemirror-5.1/addon/search/search.js',
-        './static/js/lib/codemirror-5.1/addon/dialog/dialog.js',
-        './static/js/lib/codemirror-5.1/addon/search/match-highlighter.js',
-        './static/js/lib/codemirror-5.1/addon/fold/foldcode.js',
-        './static/js/lib/codemirror-5.1/addon/fold/foldgutter.js',
-        './static/js/lib/codemirror-5.1/addon/fold/brace-fold.js',
-        './static/js/lib/codemirror-5.1/addon/fold/xml-fold.js',
-        './static/js/lib/codemirror-5.1/addon/fold/markdown-fold.js',
-        './static/js/lib/codemirror-5.1/addon/fold/comment-fold.js',
-        './static/js/lib/codemirror-5.1/addon/fold/mode/loadmode.js',
-        './static/js/lib/codemirror-5.1/addon/fold/comment/comment.js',
-        './static/js/lib/codemirror-5.1/mode/meta.js',
-        './static/js/lib/codemirror-5.1/mode/go/go.js',
-        './static/js/lib/codemirror-5.1/mode/clike/clike.js',
-        './static/js/lib/codemirror-5.1/mode/xml/xml.js',
-        './static/js/lib/codemirror-5.1/mode/htmlmixed/htmlmixed.js',
-        './static/js/lib/codemirror-5.1/mode/javascript/javascript.js',
-        './static/js/lib/codemirror-5.1/mode/markdown/markdown.js',
-        './static/js/lib/codemirror-5.1/mode/css/css.js',
-        './static/js/lib/codemirror-5.1/mode/shell/shell.js',
-        './static/js/lib/codemirror-5.1/mode/sql/sql.js',
-        './static/js/lib/codemirror-5.1/keymap/vim.js',
-        './static/js/lib/lint/json-lint.js',
-        './static/js/lib/lint/go-lint.js'];
-    gulp.src(jsLibs)
-            .pipe(uglify())
-            .pipe(concat('lib.min.js'))
-            .pipe(gulp.dest('./static/js/'));
+function minWideJS () {
+  var jsWide = [
+    './static/js/tabs.js',
+    './static/js/tabs.js',
+    './static/js/dialog.js',
+    './static/js/editors.js',
+    './static/js/notification.js',
+    './static/js/tree.js',
+    './static/js/wide.js',
+    './static/js/session.js',
+    './static/js/menu.js',
+    './static/js/windows.js',
+    './static/js/hotkeys.js',
+    './static/js/bottomGroup.js',
+  ]
+  return gulp.src(jsWide).
+    pipe(sourcemaps.init()).
+    pipe(uglify()).
+    pipe(concat('wide.min.js')).
+    pipe(sourcemaps.write('.')).
+    pipe(gulp.dest('./static/js/'))
+}
 
-    var jsWide = ['./static/js/tabs.js',
-        './static/js/tabs.js',
-        './static/js/dialog.js',
-        './static/js/editors.js',
-        './static/js/notification.js',
-        './static/js/tree.js',
-        './static/js/wide.js',
-        './static/js/session.js',
-        './static/js/menu.js',
-        './static/js/windows.js',
-        './static/js/hotkeys.js',
-        './static/js/bottomGroup.js'
-    ];
-    gulp.src(jsWide)
-            .pipe(sourcemaps.init())
-            .pipe(uglify())
-            .pipe(concat('wide.min.js'))
-            .pipe(sourcemaps.write("."))
-            .pipe(gulp.dest('./static/js/'));
-});
+gulp.task('default',
+  gulp.series(
+    gulp.parallel(minLibCSS, minZTreeStyleCSS, minWideCSS, minLibJS, minWideJS)))
