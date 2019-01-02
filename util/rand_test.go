6cdd14c7831803e1a93384a3f4f057eb99b82a61
Liang Ding <d@b3log.org>
2019-01-02 21:43:11 +0800

    :tada: :page_facing_up: 2019
    2019 新年快乐！

--- util/rand_test.go
+++ util/rand_test.go
@@ -1,4 +1,4 @@
-// Copyright (c) 2014-2018, b3log.org & hacpai.com
+// Copyright (c) 2014-2019, b3log.org & hacpai.com
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
