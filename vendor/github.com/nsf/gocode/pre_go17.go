1d42fb12154fa13f5eec83bbe9fda3a868a0c2ad
Liang Ding <d@b3log.org>
2019-05-16 09:47:09 +0800

    :heavy_minus_sign: 去掉 vendor

--- vendor/github.com/nsf/gocode/pre_go17.go
+++
@@ -1,7 +0,0 @@
-// +build !go1.7,!go1.8
-
-package main
-
-func init() {
-	knownPackageIdents["context"] = "golang.org/x/net/context"
-}
--- vendor/github.com/visualfc/gotools/doc.go
+++
@@ -1,8 +0,0 @@
-// Copyright 2011-2017 visualfc <visualfc@gmail.com>. All rights reserved.
-// Use of this source code is governed by a BSD-style
-// license that can be found in the LICENSE file.
-
-/*
-gotools document
-*/
-package main
