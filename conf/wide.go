4da07183a905384a0499a15fb2964655559f0925
Liang Ding <d@b3log.org>
2019-05-24 11:53:30 +0800

    :bookmark: 更新版本号

--- conf/wide.go
+++ conf/wide.go
@@ -39,7 +39,7 @@ const (
 	PathListSeparator = string(os.PathListSeparator)
 
 	// WideVersion holds the current Wide's version.
-	WideVersion = "1.5.3"
+	WideVersion = "1.6.0"
 	// CodeMirrorVer holds the current editor version.
 	CodeMirrorVer = "5.1"
 	// UserAgent represents HTTP client user agent.
