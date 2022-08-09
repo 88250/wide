58611c8f1ede39a52e03ed34bef54194a37c2a86
Liang Ding <d@b3log.org>
2022-08-09 10:05:05 +0800

    :art: 加入只读模式配置

--- playground/autocomplete.go
+++ playground/autocomplete.go
@@ -24,11 +24,17 @@ import (
 	"strings"
 
 	"github.com/88250/gulu"
+	"github.com/88250/wide/conf"
 	"github.com/88250/wide/session"
 )
 
 // AutocompleteHandler handles request of code autocompletion.
 func AutocompleteHandler(w http.ResponseWriter, r *http.Request) {
+	if conf.Wide.ReadOnly {
+		http.Error(w, "Forbidden", http.StatusForbidden)
+		return
+	}
+
 	var args map[string]interface{}
 	if err := json.NewDecoder(r.Body).Decode(&args); err != nil {
 		logger.Error(err)
